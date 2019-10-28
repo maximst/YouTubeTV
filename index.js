4a13d0c8b113aacf69c14bd022c6652d68371d88
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2019-10-28 13:25:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    let win = new BrowserWindow({ width: 1280, height: 720 })
+    win.AVPlayManager = {}
+    win.loadURL('https://youtube.com/tv/',
+        {
+//            userAgent: "Mozilla/5.0 (Unknown; Linux armv7l) AppleWebKit/537.1+ (KHTML, like Gecko) Safari/537.1+ LG Browser/6.00.00(+mouse+3D+SCREEN+TUNER; LGE; 42LA660S-ZA; 04.25.05; 0x00000001;); LG NetCast.TV-2013 /04.25.05 (LG, 42LA660S-ZA, wired)"
+            userAgent: 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4, SAMSUNG UKS7000) AppleWebKit/537.3 (KHTML, like Gecko) Version/2.4 TV Safari/538.1'
+        }
+    )
+
+    const ses = win.webContents.session
+    console.log(ses.getUserAgent())
+}
+
+app.on('ready', createWindow)
