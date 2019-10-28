0f71609a5cdc5baf7b6c4c59b35e0c1f73ef9346
Maxim Starodubcev <maxim@trambroid.com>
2019-10-28 21:53:59 +0200

    Fix chromium video resolution

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function createWindow() {
     win.loadURL('https://youtube.com/tv/',
         {
 //            userAgent: "Mozilla/5.0 (Unknown; Linux armv7l) AppleWebKit/537.1+ (KHTML, like Gecko) Safari/537.1+ LG Browser/6.00.00(+mouse+3D+SCREEN+TUNER; LGE; 42LA660S-ZA; 04.25.05; 0x00000001;); LG NetCast.TV-2013 /04.25.05 (LG, 42LA660S-ZA, wired)"
-            userAgent: 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4, SAMSUNG UKS7000) AppleWebKit/537.3 (KHTML, like Gecko) Version/2.4 TV Safari/538.1'
+//            userAgent: 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4, SAMSUNG UKS7000) AppleWebKit/537.3 (KHTML, like Gecko) Version/2.4 TV Safari/538.1'
+            userAgent: "Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4.0) AppleWebKit/538.1 (KHTML, like Gecko) Version/2.4.0 TV Safari/538.1"
         }
     )
 
@@ -14,4 +15,5 @@ function createWindow() {
     console.log(ses.getUserAgent())
 }
 
+app.commandLine.appendSwitch("ignore-gpu-blacklist")
 app.on('ready', createWindow)
